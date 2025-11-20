b37b4a98da2e1847390c2a72e39c4d9fe7df6479
GMoulidharan <moulidharangeo222@gmail.com>
2025-11-20 20:01:29 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -2,28 +2,24 @@ const express = require("express");
 
 const app = express(); // instance of express.js application
 
-const {adminAuth, userAuth} = require("./middlewares/auth")
 
-//handle auth Middleware for all request GET POST... requests,
-app.use("/admin", adminAuth);
-// app.use("/admin", userAuth);
 
-app.get("/user/login", userAuth, (req, res) => {
-  res.send("User logged in successfully"); 
+app.get("/getuserdata", (req, res) => {
+   try{
+      throw new Error("ncondcnds");
+      res.send("User data send");
+   }catch(err){
+      res.status(500).send("Some Error contact suport team!!")
+   }
+   res.send("User data send");
+   
 });
 
-app.get("/user", userAuth, (req, res) => {
-  res.send("User data send"); 
-});
-
-app.get("/admin/getAlData", (req, res) => {
-  //Logic of chekingAfter getting the API call check if the req is authorized(actually made by an admin)
-  res.send("All data send");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted the user");
-});
+// app.use("/",(err, req, res, next)=>{
+//    if(err){
+//       res.status(500).send("Something went wrong!!")
+//    }
+// })
 
 
 
