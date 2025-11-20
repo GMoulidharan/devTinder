9d0893db1e9e904a9ad0160709c01517ad75a0c7
GMoulidharan <moulidharangeo222@gmail.com>
2025-11-20 19:42:01 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -2,39 +2,30 @@ const express = require("express");
 
 const app = express(); // instance of express.js application
 
+const {adminAuth, userAuth} = require("./middlewares/auth")
 
-// app.use( "/user",(req, res, next) =>{
-//        console.log("Handling the route user!!");
-//       //  res.send("Response!!")
-//       next();
-//     }, 
-// )
-// app.use( "/user",(req, res, next) =>{
-//        console.log("Handling the route user!!");
-//        res.send("Independent 2nd route handler!!")
-      
-//     }, 
-// )
+//handle auth Middleware for all request GET POST... requests,
+app.use("/admin", adminAuth);
+// app.use("/admin", userAuth);
 
-app.use( "/",(req, res, next) =>{
-       console.log("Handling the route(Middle ware)!!");
-      //  res.send("Response!!")
-      next();
-    }, 
-)
-app.get( "/user",
-   (req, res, next) =>{
-       console.log("Handling the route /user (Middle ware)!!");
-      //  res.send("Response!!")
-      next();
-    }, 
-    (req, res, next) =>{
-      next();
-    },
-    (req, res) =>{
-      res.send("Finally reached route handler")
-    },
-)
+app.get("/user/login", userAuth, (req, res) => {
+  res.send("User logged in successfully"); 
+});
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("User data send"); 
+});
+
+app.get("/admin/getAlData", (req, res) => {
+  //Logic of chekingAfter getting the API call check if the req is authorized(actually made by an admin)
+  res.send("All data send");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted the user");
+});
+
+
 
 app.listen(3000, () => {
   console.log("Server is successfull listening on port 3000...");
