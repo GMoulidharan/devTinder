79d9c67bb0750155a03bdf1201263a13d87a4f37
GMoulidharan <moulidharangeo222@gmail.com>
2025-11-25 19:27:13 +0530

    Push user data to DB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express(); // instance of express.js application
+
+app.post("/signUp",async(req, res)=>{
+   const userObj ={
+      firstName:"Virat",
+      lastName:"Kholi",
+      emailId:"Virat@Kholi.com",
+      password:"Virat@123"
+   }
+
+   const user = new User(userObj);
+   try{
+      await user.save();
+   res.send("User added Successfully")
+   }catch(err){
+      res.status(400).send("Error saving the User: " + err.message);
+   }
+   
+})
+
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async() =>{
-    await mongoose.connect("mongodb+srv://moulidharansrv_db_user:jHpfmlt8vzM1bE3q@node.lwcttun.mongodb.net/")
+    await mongoose.connect("mongodb+srv://moulidharansrv_db_user:jHpfmlt8vzM1bE3q@node.lwcttun.mongodb.net/devTinder")
 }
 
 module.exports = connectDB;
